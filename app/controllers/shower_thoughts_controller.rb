ce3b8634183f592b1ae27108da82ba46b8c6bdc8
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-18 13:35:35 -0500

    index page create

--- app/controllers/shower_thoughts_controller.rb
+++ app/controllers/shower_thoughts_controller.rb
@@ -1,2 +1,24 @@
 class ShowerThoughtsController < ApplicationController
+  def index
+    @shower_thoughts = ShowerThought.all
+    return "index.html.erb"
+  end
+  def new
+    return "new.html.erb"
+  end
+  def create
+    redirect_to "index.html.erb"
+  end
+  def show
+    return "show.html.erb"
+  end
+  def edit
+    return "edit.html.erb"
+  end 
+  def update
+    redirect_to "index.html.erb"
+  end
+  def destroy
+    redirect_to "index.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get "/" => "showerthoughts#index"
+ get "/showerthoughts" => "showerthoughts#index"
+ get "/showerthoughts/new" => "showerthoughts#new"
+ post "/showerthoughts" => "showerthoughts#create"
+ get "/showerthoughts/:id" => "showerthoughts#show"
+ get "/showerthoughts/:id/edit" => "showerthoughts#edit"
+ patch "/showerthoughts/:id" => "showerthoughts#update"
+ delete "/showerthoughts/:id" => "showerthoughts#destroy"
 end
